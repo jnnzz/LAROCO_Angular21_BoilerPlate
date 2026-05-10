64562445c335cd7846b667a45d6f1d975b4aafda
jnnzz <larocojanlorenz@gmail.com>
2026-05-11 01:05:36 +0800

    feat(account): add verify-email component

---
+++ src/app/account/verify-email.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '../_services/account.service';
+import { AlertService } from '../_services/alert.service';
+
+enum EmailStatus {
+    Verifying,
+    Failed
+}
+
+@Component({ templateUrl: 'verify-email.component.html', standalone: false })
+export class VerifyEmailComponent implements OnInit {
+    EmailStatus = EmailStatus;
+    emailStatus = EmailStatus.Verifying;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService,
+        private cdr: ChangeDetectorRef
+    ) {}
+
+    ngOnInit() {
+        const token = this.route.snapshot.queryParams['token'];
+
+        this.router.navigate([], { relativeTo: this.route, replaceUrl: true });
+
+        this.accountService.verifyEmail(token)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.alertService.success('Verification successful, you can now login', { keepAfterRouteChange: true });
+                    this.router.navigate(['../login'], { relativeTo: this.route });
+                },
+                error: () => {
+                    this.emailStatus = EmailStatus.Failed;
+                    this.cdr.detectChanges();
+                }
+            });
+    }
+}
